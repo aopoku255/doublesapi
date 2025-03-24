0021925446d989a9a61382f1301039ccb8192eb3
aopoku255 <aopoku255@gmail.com>
2025-03-24 17:32:16 +0000

    db connection

--- database/database.js
+++ database/database.js
@@ -1,6 +1,10 @@
 const { default: mongoose } = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost:27017/doubles")
+  .connect(
+    "mongodb+srv://aopoku255:5mhjojv3w6uCL2hG@cluster0.ltmnhp4.mongodb.net/doubles"
+  )
   .then((res) => console.log("Mongodb connected"))
   .catch((err) => console.log("An error occured when connnecting to db"));
+
+// .connect("mongodb://localhost:27017/doubles")
