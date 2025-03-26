f59eb8ce2a4aa5f59bb2b5a0e7607bac8b6b7a30
aopoku255 <aopoku255@gmail.com>
2025-03-26 14:09:34 +0000

    serving images

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const path = require("path");
 const router = require("./routes/router");
 require("./database/database");
 const app = express();
@@ -7,6 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 app.use(morgan("short"));
 app.use(express.json());
+
+// Serve the uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/v1", router);
 app.listen(PORT, () => console.log(`APP running on port ${PORT}`));
