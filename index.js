94f4eda72fd63556d105d18684c67f8954896cae
aopoku255 <aopoku255@gmail.com>
2025-06-12 03:42:41 +0000

    port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 // require("./database/database");
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 app.use(morgan("short"));
 app.use(express.json());
 app.use(
