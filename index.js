619a5fedb834528931dd0813d782d30301b81c0e
aopoku255 <aopoku255@gmail.com>
2025-06-12 09:23:32 +0000

    Container fix

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const morgan = require("morgan");
 const path = require("path");
 const router = require("./routes/router");
 const cors = require("cors");
-// require("./database/database");
+
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+// Cloud Run provides PORT as an environment variable
+const PORT = process.env.SERVERPORT || 3001;
+
 app.use(morgan("short"));
 app.use(express.json());
 app.use(
@@ -19,4 +21,5 @@ app.use(
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/v1", router);
+
 app.listen(PORT, () => console.log(`APP running on port ${PORT}`));
